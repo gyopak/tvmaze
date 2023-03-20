d1b566075282f6ad3a02c37e0ef8bebc9228f943
gyuri <gyuri@gyuris-MacBook-Air.local>
2023-03-20 15:31:37 +0100

    feat: shows by category

--- src/features/showsByCategory.js
+++ src/features/showsByCategory.js
@@ -1,6 +1,21 @@
+import { useMemo } from "react"
 import { useGetShowsQuery } from "./showsApi"
 
 export const useShowsByCategory = () => {
   const { data: shows, isLoading } = useGetShowsQuery()
-  return { shows, isLoading }
+
+  const showsByCategory = useMemo(() => (
+    // create an object with category keys
+    // a show can have multiple category
+    shows?.reduce((previousCategories, currentShow) => {
+      const categories = {  ...previousCategories }
+      const currentCategories = currentShow?.genres || []
+      currentCategories.forEach(category => {
+        const showsInCategory = categories[category]
+        categories[category] = !showsInCategory ? [currentShow] : [...showsInCategory, currentShow] 
+      })
+      return categories
+    }, {})
+  ), [shows])
+  return { shows: showsByCategory, isLoading }
 }
